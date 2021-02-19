ed58682faa26612f0bae036228f47ce40bb72086
Brian Underwood <brian@brian-underwood.codes>
2021-02-19 20:36:48 +0100

    Build to docs to make things easier, even though they aren't docs 🤷‍♂️

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,109 @@
+var store = [{
+        "title": "A Blog Post Toward a Programatically Generated Philisophical Language",
+        "excerpt":"“Wilkins! Yes! I’ve considered decorating these walls with some graffiti of my own, and writing it in the Universal Character.. but it is too depressing. ‘Look, we have invented a new Philisophical Language so that when we are imprisoned by Kings we can scratch a higher form of graffiti on...","categories": ["investigations","philisophical language"],
+        "tags": [],
+        "url": "/investigations/philisophical%20language/2014/07/20/a_blog_post_towards_a_programatically_generated_philisophical_language",
+        "teaser": null
+      },{
+        "title": "Normalizing Religion in Ireland",
+        "excerpt":"When I told the people of Northern Ireland that I was an atheist, a woman in the audience stood up and said, ‘Yes, but is it the God of the Catholics or the God of the Protestants in whom you don’t believe?’ – Quentin Crisp I’ve been staying at an...","categories": ["investigations","neo4j","irish census"],
+        "tags": [],
+        "url": "/investigations/neo4j/irish%20census/2014/09/02/normalizing_religion_in_ireland",
+        "teaser": null
+      },{
+        "title": "Matchmaking Irish Citizens",
+        "excerpt":"I am he as you are he as you are me and we are all together – The Beatles For a while now I have been working in my spare time on a project involving the 1901 and 1911 Irish national censuses. I have been casually exploring the data, but...","categories": ["investigations","neo4j","irish census"],
+        "tags": ["neo4j"],
+        "url": "/investigations/neo4j/irish%20census/2014/10/26/matchmaking_irish_citizens",
+        "teaser": null
+      },{
+        "title": "neo4apis",
+        "excerpt":"I’ve been reading a few interesting analyses of Twitter data recently such as this #gamergate analysis by Andy Baio. I thought it would be nice to have a mechanism for people to quickly and easily import data from Twitter to Neo4j for research purposes. Like a good programmer I had...","categories": ["ruby","neo4j","announcements","neo4apis"],
+        "tags": [],
+        "url": "/ruby/neo4j/announcements/neo4apis/2014/11/03/neo4apis",
+        "teaser": null
+      },{
+        "title": "Analyzing Twitter with Neo4j and Rails",
+        "excerpt":"Having recently become interested in making it easy to pull data from Twitter with neo4apis-twitter I also decided that I wanted to be able to visualize and browse the data that I’ve downloaded. Therefore I created the twitter_analytics rails app! What cool stuff does this do? Dashboard My first goal...","categories": ["ruby","rails","neo4j","announcements"],
+        "tags": ["neo4j","tools","twitter","neo4apis"],
+        "url": "/ruby/rails/neo4j/announcements/2014/12/17/analyzing_twitter_with_neo4j_and_rails",
+        "teaser": null
+      },{
+        "title": "Loading SQL to Neo4j Like Magic",
+        "excerpt":"When using neo4j for the first time, most people want to import data from another database to start playing around. There are a lot of options including LOAD CSV, batch-import, and even using Groovy. All of these require some setup and configuration. I wanted to create the simplest SQL to...","categories": ["sql","neo4j","neo4apis","activerecord","announcements"],
+        "tags": ["neo4j","tools","sql"],
+        "url": "/sql/neo4j/neo4apis/activerecord/announcements/2015/01/08/loading_sql_to_neo4j_like_magic",
+        "teaser": null
+      },{
+        "title": "Modeling in Neo4j Sans Programming",
+        "excerpt":"I have a bit of a problem. Sometimes I have a hard time implementing a specific solution to a problem. Sometimes I think: “If I just took a step back, I could create a tool which anybody could use to do anything!” If I do it right, nobody will ever...","categories": ["ruby","rails","neo4j","announcements"],
+        "tags": ["neo4j","tools","neo4j.rb"],
+        "url": "/ruby/rails/neo4j/announcements/2015/02/04/modeling_in_neo4j_sans_programming",
+        "teaser": null
+      },{
+        "title": "Making Master Data Management Fun with Neo4j - Part 1",
+        "excerpt":"Joining multiple disparate data-sources, commonly dubbed Master Data Management (MDM), is usually not a fun exercise. I would like to show you how to use a graph database (Neo4j) and an interesting dataset (developer-oriented collaboration sites) to make MDM an enjoyable experience. This approach will allow you to quickly and...","categories": [],
+        "tags": ["neo4j","master-data-management","clojure","stackoverflow"],
+        "url": "/2015/02/16/making_master_data_management_fun_with_neo4j_-_part_1",
+        "teaser": null
+      },{
+        "title": "Making Master Data Management Fun with Neo4j - Part 2",
+        "excerpt":"In my last post I said I would “bring in another data source, show how I linked the data together, and demonstrate the sort of bigger picture that one can get from this approach”. There’s a lot to talk about, so I’m going to break these each into different posts...","categories": [],
+        "tags": ["neo4j","master-data-management","github"],
+        "url": "/2015/02/22/making_master_data_management_fun_with_neo4j_-_part_2",
+        "teaser": null
+      },{
+        "title": "Making Master Data Management Fun with Neo4j - Part 3",
+        "excerpt":"In my last two posts I covered the process of importing data from StackOverflow and GitHub for the purpose of creating a combined MDM database. Now we get to the really fun stuff: today I show you how I tied the data together! Tying the data together The most obvious...","categories": [],
+        "tags": ["neo4j","master-data-management","phash","r","stackoverflow","github"],
+        "url": "/2015/03/08/making_master_data_management_fun_with_neo4j_-_part_3",
+        "teaser": null
+      },{
+        "title": "Running a Neo4j Cypher Introduction Workshop",
+        "excerpt":"Last night I ran a very successful workshop at the Friends of Neo4j Stockholm meetup group. The format was based on a workshop that I attended in San Francisco while the World Cup was going on and was based around investigating a dataset of previous World Cup matches. Roughly the...","categories": [],
+        "tags": ["neo4j","advocacy"],
+        "url": "/2015/04/16/running_a_neo4j_intro_workshop",
+        "teaser": null
+      },{
+        "title": "Master Data Management Scoring Examples",
+        "excerpt":"A while ago my colleague Michael suggested to me that I draw out some examples of how my record linkage algorithm did it’s thing. In order to do that, I’ve picked some users from both GitHub and StackOverflow which are big names in the Neo4j community to give you the...","categories": [],
+        "tags": ["neo4j","master-data-management","stackoverflow","github","analysis"],
+        "url": "/2015/05/14/master_data_management_scoring_examples",
+        "teaser": null
+      },{
+        "title": "Analyzing Ruby's ObjectSpace with Neo4j",
+        "excerpt":"Recently the continuous builds for the neo4j Ruby gem failed for JRuby because the memory limit had been reached. I wanted to see if I could use my favorite tool (Neo4j) to analyize the memory usage. So I threw together a bit of Ruby code to use Ruby’s ObjectSpace.each_object functionality...","categories": [],
+        "tags": ["neo4j","ruby","analysis","memory"],
+        "url": "/2015/06/03/analyzing_rubys_objectspace_with_neo4j",
+        "teaser": null
+      },{
+        "title": "Using Graph Structure Record Linkage on Irish Census Data with Neo4j",
+        "excerpt":"For just over a year I’ve been obsessed on-and-off with a project ever since I stayed in the town of Skibbereen, Ireland. Taking data from the 1901 and 1911 Irish censuses I hoped I would be able to find a way to reliably link resident records from the two together...","categories": [],
+        "tags": ["neo4j","analysis","master-data-management"],
+        "url": "/2015/08/20/using_graph_structure_record_linkage_on_irish_census_data_with_neo4j",
+        "teaser": null
+      },{
+        "title": "GraphStarter: Getting a Neo4j Rails app up and running quickly",
+        "excerpt":"For a while now I’ve been building various Neo4j.rb educational resources using the example of an asset portal. There has been: a screencast series (the first half and the second half) a SitePoint article on building recommendations and access control a webinar on advanced access control a introduction course for...","categories": [],
+        "tags": ["neo4j","neo4j.rb","ruby gem"],
+        "url": "/2015/10/27/graph_starter",
+        "teaser": null
+      },{
+        "title": "Analyzing Ruby Code with Neo4j",
+        "excerpt":"For a long time I’ve been wanting to use Neo4j as a tool to analyze Ruby code. Using Ruby for almost a decade, I have a lot of experience with effectively finding my way around Ruby code. Still Ruby programs can by very dynamic and thus tools are always welcome....","categories": ["ruby","rails","neo4j"],
+        "tags": ["neo4j","tools","neo4j.rb"],
+        "url": "/ruby/rails/neo4j/2016/01/11/analyzing_ruby_code_with_Neo4j",
+        "teaser": null
+      },{
+        "title": "Expecting the Unexpected in Elixir",
+        "excerpt":"Apollo 11's Saturn V rocket on the launchpad at the Kennedy Space Center. 1 July 1969. Photo: NASA What can a 50 year old cryptic error message teach us about the software we write today? Cross-posted from Medium On July 24, 1969 Neil Armstrong and Buzz Aldrin were attempting to...","categories": ["elixir"],
+        "tags": ["elixir","ksp"],
+        "url": "/elixir/2020/01/31/expecting-the-unexpected-in-elixir",
+        "teaser": null
+      },{
+        "title": "Structuring an Elixir+Phoenix App",
+        "excerpt":"I’ve mix phx.new ed many applications and when doing so I often start with wondering how to organize my code. I love how Phoenix pushes you to think about the different domains in your app via generators while at the same time I have the freedom to organize modules on...","categories": ["elixir"],
+        "tags": ["elixir","phoenix","ecto"],
+        "url": "/elixir/2020/07/17/structuring-an-elixir+phoenix-app",
+        "teaser": null
+      }]
